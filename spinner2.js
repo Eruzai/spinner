8e5837e4e1add27e4b9c538ef7cb9c3ec9580ce0
Kevin Levasseur <kevin_levasseur@telus.net>
2024-01-17 10:30:28 -0800

    refactored code

--- spinner2.js
+++ spinner2.js
@@ -1,30 +1,9 @@
-setTimeout(() => {
-  process.stdout.write('\r|   ');
-}, 100);
-setTimeout(() => {
-  process.stdout.write('\r/   ');
-}, 300);
-setTimeout(() => {
-  process.stdout.write('\r-   ');
-}, 500);
-setTimeout(() => {
-  process.stdout.write('\r\\   ');
-}, 700);
-setTimeout(() => {
-  process.stdout.write('\r|   ');
-}, 900);
-setTimeout(() => {
-  process.stdout.write('\r/   ');
-}, 1100);
-setTimeout(() => {
-  process.stdout.write('\r-   ');
-}, 1300);
-setTimeout(() => {
-  process.stdout.write('\r\\   ');
-}, 1500);
-setTimeout(() => {
-  process.stdout.write('\r|   ');
-}, 1700);
-setTimeout(() => {
-  process.stdout.write('\n');
-}, 1900);
+const animationItems = ['\r|   ', '\r/   ', '\r-   ', '\r\\   ', '\r|   ', '\r/   ', '\r-   ', '\r\\   ', '\r|   ', '\n'];
+let delay = 100;
+
+for (const item of animationItems) {
+  setTimeout(() => {
+    process.stdout.write(item);
+  }, delay);
+  delay += 200;
+}
